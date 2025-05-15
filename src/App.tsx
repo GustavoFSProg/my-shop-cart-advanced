f5e04f8bf67284ea3b272cfc6bb0a8f99645e96c
GustavoFSProg <gustavo.prog40@gmail.com>
2025-05-15 19:02:51 -0300

    navbar -1

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Store } from './pages/Store'
 import { About } from './pages/About'
+import { Navbar } from './components/Navbar'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
+    <Navbar />
      <Container className="mb-4">
       <Routes >
         <Route path="/"  element={<Home />}/>
