dc4487adfa79b61544dcc912583bcb9bff18eee3
GustavoFSProg <gustavo.prog40@gmail.com>
2025-05-16 10:38:40 -0300

    home change

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,18 @@
+import { Col, Row } from "react-bootstrap"
+import { StoreItem } from "../components/StoreItem"
+import storeItems from "../data/items.json"
 
- 
- export function Home(){
-    return(
-        <>
-        <h1>HOME</h1>
-            </>
-    )
- }
+export function Home() {
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map(item => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
